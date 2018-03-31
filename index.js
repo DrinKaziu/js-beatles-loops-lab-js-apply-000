bf01e7b43413e2b75a62899c9ff65e143fd6e28f
Drin K <drinkaziu@gmail.com>
2018-03-31 16:20:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,31 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
-  var arr = [];
+  var beatleInstArr = [];
   for (var i = 0; i < musicians.length; i++) {
-    var musician = musicians[i];
-    arr.push(musician + ' ' + 'plays ' + instruments[i]);
+    beatleInstArr.push(musicians[i] + ' ' + 'plays ' + instruments[i]);
   }
   
   return arr;
 }
 
-function johnLennonFacts(fact) {
+
+const facts = [
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+        ];
+        
+function johnLennonFacts(facts) {
   var factsArr = [];
+  for (var i = 0; i < facts.length; i++) {
+    factsArr.push(facts[i] + '!!!');
+  }
+  
+  return factsArr;
 }
+
+
+
+
