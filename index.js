8635e451df6e05b5a56fda2abfa8b65e80486c60
Drin K <drinkaziu@gmail.com>
2018-03-31 16:28:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,20 @@ const facts = [
         "He hated the sound of his own voice"
         ];
         
+const facts = [
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+        ];
+        
 function johnLennonFacts(facts) {
   var factsArr = [];
-  for (var i = 0; i < facts.length; i++) {
+  var i = 0;
+  
+  while (i < facts.length) {
     factsArr.push(facts[i] + '!!!');
+    i++;
   }
   
   return factsArr;
